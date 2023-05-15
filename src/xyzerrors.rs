3b5b67181436aad4eb070c5898da85b35c5d2dea
Timo Schulz <timo.schulz@hhu.de>
2023-05-15 21:26:09 +0200

    Added error type CoordinateError to ParseXYZErrors, which might be raised if a float in the file cannot be parsed correctly. This can have various reasons, such as no or a wrong seperator.

--- src/xyzerrors.rs
+++ src/xyzerrors.rs
@@ -1,8 +1,11 @@
 use std::error;
-use std::{fmt::Display, num::ParseIntError};
+use std::{fmt::Display, num::ParseFloatError, num::ParseIntError};
 
+#[derive(Debug)]
 pub enum ParseXYZError {
+    EmptyLine,
     WrongHeaderError(ParseIntError),
+    CoordinateError(ParseFloatError),
 }
 
 impl Display for ParseXYZError {
@@ -14,6 +17,12 @@ impl Display for ParseXYZError {
                     "Could not parse the first line as integer. Does it start with the number of atoms?"
                 )
             }
+            ParseXYZError::CoordinateError(_) => {
+                write!(f, "Could not parse the coordinate. Is it a correct float? Is the seperator a whitespace?")
+            }
+            ParseXYZError::EmptyLine => {
+                write!(f, "Found an empty line where there shouldn't be one.")
+            }
         }
     }
 }
@@ -22,10 +31,11 @@ impl error::Error for ParseXYZError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match *self {
             ParseXYZError::WrongHeaderError(ref e) => Some(e),
+            ParseXYZError::CoordinateError(ref e) => Some(e),
+            ParseXYZError::EmptyLine => None,
         }
     }
 }
-
 
 impl From<ParseIntError> for ParseXYZError {
     fn from(value: ParseIntError) -> Self {
